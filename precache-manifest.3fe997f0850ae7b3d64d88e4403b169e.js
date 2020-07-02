4d69322020b5cf9526ec9e5e8afa32aec48b4399
Arun Bagga <abarunbagga12@gmail.com>
2020-07-02 17:50:57 +0530

    Updates

---
+++ precache-manifest.3fe997f0850ae7b3d64d88e4403b169e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d865904c8f7c4701e2e3ed4492f977",
+    "url": "/MyStore/index.html"
+  },
+  {
+    "revision": "bfbdbd91d703736cfdca",
+    "url": "/MyStore/static/css/2.e7aff499.chunk.css"
+  },
+  {
+    "revision": "3cbd58e68e8357b637fe",
+    "url": "/MyStore/static/css/main.e3864a8f.chunk.css"
+  },
+  {
+    "revision": "bfbdbd91d703736cfdca",
+    "url": "/MyStore/static/js/2.3205cc6a.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/MyStore/static/js/2.3205cc6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbd58e68e8357b637fe",
+    "url": "/MyStore/static/js/main.741f405b.chunk.js"
+  },
+  {
+    "revision": "c33ac94b67c35cbc3df1",
+    "url": "/MyStore/static/js/runtime-main.1897bdec.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MyStore/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MyStore/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MyStore/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MyStore/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MyStore/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
